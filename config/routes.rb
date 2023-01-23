fa2e2cf4cd4e833c31be483ab98ce51b222dda83
jbdevelop3r <justinebanogon14@gmail.com>
2023-01-23 15:00:38 +0800

    api

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
-  
-  
-  resources :composers do
-    resources :albums
-    resources :songs
+
+root to: "api/v1/composers#index"
+
+  namespace :api do 
+    namespace :v1 do
+      resources :composers do
+        resources :albums
+        resources :songs
+      end
+    end
   end
+
+
+  namespace :v2 do
+    resources :composers do
+      resources :albums
+      resources :songs
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
